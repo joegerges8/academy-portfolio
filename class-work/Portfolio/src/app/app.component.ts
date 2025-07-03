a50b1b81d4f76549cc8d01fae8e5f3a313535714
joegerges8 <joe.gerges2004@gmail.com>
2025-07-03 14:39:22 +0300

    About section finished

--- class-work/Portfolio/src/app/app.component.ts
+++ class-work/Portfolio/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from './navbar/navbar.component';
 import {ProfileComponent} from './profile/profile.component'
+import {AboutComponent} from './about/about.component'
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, ProfileComponent],
+  imports: [RouterOutlet, NavbarComponent, ProfileComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
